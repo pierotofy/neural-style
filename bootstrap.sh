86ee86d57ce6e111ccc2d2c244dace2e6a3bd8f7
Piero Toffanin <pt@masseranolabs.com>
2017-07-13 17:58:20 -0400

    Added sudoers

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,9 @@
 # To execute run:
 # wget -O - https://raw.githubusercontent.com/pierotofy/neural-style/master/bootstrap.sh | bash
 
+me=$(whoami)
+sudo echo "$me ALL=(ALL) NOPASSWD: /sbin/poweroff, /sbin/reboot, /sbin/shutdown" >> /etc/sudoers
+
 # Install neural style and dependencies on a paperspace ML-box
 pip install requests
 git clone https://github.com/pierotofy/neural-style
